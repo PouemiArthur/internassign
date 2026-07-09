c788d19b8cd8663f9fcaa20045c5757bc9e96a62
PouemiArthur <pouemijunior@gmail.com>
2026-07-09 16:19:51 +0100

    initial commit

---
+++ routes/proff.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { setAvailability, updateAvailability, cancelAppointments } from "../controllers/proff.js";
+import { authMiddleware } from "../middleware/auth.js"; 
+import { profMiddleware } from "../middleware/auth.js"; 
+const router = express.Router();
+
+
+router.post("/availability", authMiddleware,profMiddleware, setAvailability);
+
+
+// Route to cancel appointments between professor and student - Only accessible to professors
+
+router.patch("/availablility/:availabilityid", authMiddleware, profMiddleware, updateAvailability);
+router.patch("/cancel-appointments/:studentId", authMiddleware, profMiddleware, cancelAppointments);
+
+export default router;
